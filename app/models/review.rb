922e854de14a2cb3cd54686636a826ad08a8ab03
Eric Lee <erlee92@yahoo.com>
2024-01-22 15:17:14 -0500

    render test again

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,7 @@
 class Review < ApplicationRecord
+    validates :user_id, :trail_id, :rating, :review, presence: true
+    validates :rating, inclusion: {in: 1..5, message: "Rating must be selected"}
 
+    belongs_to :user
+    belongs_to :trail
 end
