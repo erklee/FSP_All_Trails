d75973141af28e66d32b8f996fb3f9526c6098ef
Eric Lee <erlee92@yahoo.com>
2024-01-22 15:11:04 -0500

    fix reviews table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_22_164434) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_22_200915) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,15 +42,15 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_22_164434) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "create_reviews", force: :cascade do |t|
+  create_table "reviews", force: :cascade do |t|
     t.text "review", null: false
     t.integer "rating", null: false
     t.bigint "user_id", null: false
     t.bigint "trail_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["trail_id"], name: "index_create_reviews_on_trail_id"
-    t.index ["user_id"], name: "index_create_reviews_on_user_id"
+    t.index ["trail_id"], name: "index_reviews_on_trail_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "trails", force: :cascade do |t|
@@ -81,6 +81,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_22_164434) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "create_reviews", "trails"
-  add_foreign_key "create_reviews", "users"
+  add_foreign_key "reviews", "trails"
+  add_foreign_key "reviews", "users"
 end
