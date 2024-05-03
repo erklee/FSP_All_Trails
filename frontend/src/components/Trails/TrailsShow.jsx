1e31646861e6b64f2c44a2a003f92694331724e7
Eric Lee <erlee92@yahoo.com>
2024-05-02 22:13:42 -0400

    weather card

--- frontend/src/components/Trails/TrailsShow.jsx
+++ frontend/src/components/Trails/TrailsShow.jsx
@@ -45,7 +45,34 @@ function TrailShow() {
         showWeather()
     }, [trail?.lat, trail?.lon])
 
-    console.log(weather)
+    // console.log(weather)
+
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    initialSlide: 1
+                }
+            }
+        ]
+    };
     return(
         <>
             <div className="parent-show-wrapper">
